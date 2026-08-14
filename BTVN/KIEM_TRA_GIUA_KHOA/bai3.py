7b06c8ed7935e70e6e1d894ff93c01c6085194d9
Doan Thi Huyen Thu <thudoan745@gmail.com>
2026-08-14 07:10:14 +0700

    finish btvnb5

--- BTVN/KIEM_TRA_GIUA_KHOA/bai3.py
+++ BTVN/KIEM_TRA_GIUA_KHOA/bai3.py
@@ -1,12 +1,12 @@
-def tim_tu(chuoi, target):
+def tim_tu(chuoi, tu_can_tim):
     words = chuoi.split()
     found = False
     for i in range(len(words)):
-        if words[i] == target:
+        if words[i] == tu_can_tim:
             print(i)
             found = True
     if found == False:
         print(-1)
 chuoi = input("Nhap chuoi: ")
-target = input("Nhap tu can tim: ")
-tim_tu(chuoi, target)
+tu_can_tim = input("Nhap tu can tim: ")
+tim_tu(chuoi, tu_can_tim)
